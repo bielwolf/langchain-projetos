505cdd9e80cd0f23b00d1d701f470503e303b984
bielwolf <gabryellobo@gmail.com>
2026-03-27 14:28:08 -0300

    update: atualiazação do pacote do langchain usado part-1

--- Projeto_farmaceutico/src/main.py
+++ Projeto_farmaceutico/src/main.py
@@ -4,24 +4,53 @@ import os
 from dotenv import load_dotenv
 
 # Loader responsável por ler arquivos PDF
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 
 # Responsável por dividir textos grandes em chunks menores
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Classe que transforma texto em embeddings vetoriais
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 # Banco vetorial para armazenamento e busca semântica
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 
 # Modelo de linguagem conversacional
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 # Cadeia pronta de Perguntas e Respostas com RAG
-from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI
 
 # Carrega as variáveis de ambiente
 load_dotenv()
 chaveApi = os.getenv("CHAVE_API")
+
+# Caminho para os arquivos
+base_path = os.path.join(os.getcwd(), "data")
+caminhos_bules = [
+    os.path.join(base_path, "dipirona.pdf"),
+    os.path.join(base_path, "paracetamol.pdf")
+]
+
+# Lista que armazena os documentos
+documentos = []
+
+# Percorre os arquivos PDF
+for caminho in caminhos_bules:
+    # Cria o loader
+    loader = PyPDFLoader(caminho)
+
+    # Carrega os documentos
+    docs = loader.load()
+
+    # Adiciona o nome do medicamento como metadata
+    for doc in docs:
+        doc.metadata["medicamento"] = caminho.split("/")[-1].replace(".pdf", "")
+
+    # Adiciona os documentos a lista
+        documentos.extend(docs)
+
+print(len(documentos))
+
+
 
